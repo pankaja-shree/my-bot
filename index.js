0c1a2950e9c9db146a789f4622cdc10b09953710
Pankajashree <pankajashree@gmail.com>
2017-04-27 20:18:03 +0530

    restore to default

--- index.js
+++ index.js
@@ -76,7 +76,6 @@ function sendTextMessage(sender, text) {
 }
 
 function sendGenericMessage(sender) {
-
     let messageData = {
 	    "attachment": {
 		    "type": "template",
@@ -87,9 +86,13 @@ function sendGenericMessage(sender) {
 				    "subtitle": "Element #1 of an hscroll",
 				    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
 				    "buttons": [{
-					    "type": "RandomGif",
-					    "title": "Show random Gif",
-					    "payload": 'img src=',
+					    "type": "web_url",
+					    "url": "https://www.messenger.com",
+					    "title": "web url"
+				    }, {
+					    "type": "postback",
+					    "title": "Postback",
+					    "payload": "Payload for first element in a generic bubble",
 				    }],
 			    }, {
 				    "title": "Second card",
